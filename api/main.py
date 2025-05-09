46d8bf0b63239e87b434f68b5a4aea1265965902
Muyiwa Abidoye <79062706+Muyiiwaa@users.noreply.github.com>
2025-05-09 16:13:16 +0000

    added schema

--- api/main.py
+++ api/main.py
@@ -12,6 +12,17 @@ app = FastAPI(
 def root():
     return {"message": "we are live!!"}
 
+# an endpoint that greets people
+@app.post("/greet/")
+def greet(name: str, year_birth: int):
+    age = 2025 - year_birth
+    return {
+        "greeting": f"Hi {name} how are you?",
+        "age": f"Wow, you are {age} years old"
+    }
+
+
+
 if __name__ == "__main__":
     uvicorn.run("main:app", port = 8000, host = "localhost", reload = True)
 
