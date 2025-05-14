c6e7fa935d6871164a7139a444e04e78f57f72ce
Muyiwa Abidoye <79062706+Muyiiwaa@users.noreply.github.com>
2025-05-14 16:43:02 +0000

    final basic commit

--- api_basics/main.py
+++ api_basics/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 import uvicorn
-from schema import RootResponse
+from schema import RootResponse,OrderRequest,OrderResponse
+from utils import get_order,get_menu
 
 
 
@@ -11,7 +12,7 @@ app = FastAPI(
     version= "v1"
 )
 
-@app.get('/',response_model=RootResponse)
+@app.get('/',response_model=RootResponse, tags=["Health"])
 async def root():
     """
     This is the root endpoint that serves as the app homepage.
@@ -19,6 +20,21 @@ async def root():
     """
     return RootResponse(
         message= "Appclick Restaurant is live!"
+    )
+
+
+@app.post("/order/", response_model=OrderResponse, tags=["Restaurant"])
+def get_orders(order: OrderRequest):
+    """
+    This endpoint accepts user order, process it and returns the
+    total cost of their order. It expects the exact payload in the
+    schema and returns a string object of the total cost.
+    """
+    user_order = order.model_dump()
+    total_cost = get_order(**user_order)
+
+    return OrderResponse(
+        total_bill= f'${total_cost}'
     )
 
 if __name__ == "__main__":
--- api_basics/schema.py
+++ api_basics/schema.py
@@ -4,3 +4,17 @@ from pydantic import BaseModel,Field
 # create root response
 class RootResponse(BaseModel):
     message: str = Field(..., examples=["Appclick Restaurant is live!"])
+
+# create order request
+class OrderRequest(BaseModel):
+    rice: int = Field(..., description="The portion of rice",
+                       examples=[0])
+    beans: int = Field(..., description="The portion of beans",
+                       examples=[0])
+    beef: int = Field(..., description="The Number of beef",
+                       examples=[0])
+    chicken: int = Field(..., description="The Number of chicken",
+                       examples=[0])
+
+class OrderResponse(BaseModel):
+    total_bill: str = Field(..., examples=["$0.0"])
